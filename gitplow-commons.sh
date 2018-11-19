b71bb9d2f716ea98ac1854330c4e1da63362be24
Daniele Bellavista <daniele.bellavista@gmail.com>
2018-11-19 13:10:44 +0100

    WIP

--- gitplow-commons.sh
+++ gitplow-commons.sh
@@ -72,3 +72,39 @@ merge_request() {
         -d "remove_source_branch=$REMOVE" \
         "https://${DOMAIN}/api/v4/projects/${PROJECTID}/merge_requests"
 }
+
+accept_request() {
+    MRID=$1
+
+    PROJECTID=$(get_gitlab_prid)
+    PRIVATETOKEN=$(get_gitlab_token)
+    DOMAIN=$(get_gitlab_domain)
+
+    curl --header "Private-Token: $PRIVATETOKEN"\
+        -X PUT "https://${DOMAIN}/api/v4/projects/${PROJECTID}/merge_requests/${MRID}/merge"
+}
+
+pull() {
+  echo "Pulling $1"
+  git checkout $1
+  git pull origin $1
+}
+
+approveMasterDevelop() {
+  local NAME="$1"; shift
+  local MR1_ID="$1"; shift
+  local MR2_ID="$1"; shift
+  if [[ -z "$MR1_ID" ]] || [[ -z "$MR2_ID" ]]; then
+    echo "Missing merge requests ids"
+    return 1
+  fi
+  set -e
+  echo "Accepting request $MR1_ID"
+  accept_request $MR1_ID
+  echo "Accepting request $MR2_ID"
+  accept_request $MR2_ID
+  pull "develop"
+  pull "master"
+  git tag $NAME -e
+  git push --tags
+}
